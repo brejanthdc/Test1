5a5309f3c1bc28f01652c518437079fefd054afb
Brejanth Rajendran <Brejanth.rajendran@dcmail.ca>
2020-01-30 20:40:12 -0500

    Commit 4, part 2 started, comments are in

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,21 +12,25 @@ let app;
 (function(app){
 
     // Declare Function Variables here...
-   
-    
+   //create a h3 tag
+    let h3 = document.createElement("h3");
 
-    
+    //link the contentArea tag to a var
+    let contentArea = document.getElementById("contentArea");
+
+    //Link the large button to a var
+    let ButtonName = document.getElementById("largeButton");
+     //Link the firstParagraph paragraph to a var
+    let Paragraph1 = document.getElementById("firstParagraph");
+
+   
 
     
     /**
      * Variable initialization in this function
      *
      */
-    let h3 = document.createElement("h3");
-    let contentArea = document.getElementById("contentArea");
-
-    let ButtonName = document.getElementById("largeButton");
-    let Paragraph1 = document.getElementById("firstParagraph");
+    
     function Start()
     {
 
@@ -41,19 +45,43 @@ let app;
     function Main()
     {
         console.log("%cDeclaring Variables", "color: red;");
+        //changes the text content of the button
        ButtonName.textContent ="Learn More";
       
+       //sets the attribute for the h3 tag, then changes the text
        h3.setAttribute("class", "display-6");
        h3.textContent =" [Brejanth Rajendran] [100557484].";
+       //adds the h3 tag to the main content area
        contentArea.appendChild(h3);
-
-       firstParagraph.textContent ="JavaScript is a prototype-based language, and every object in  JavaScript has a hidden internal property called Prototype that can be used to extend object properties and methods"
-       firstParagraph.style.visibility = "hidden";
-       
-       console.log(firstParagraph);
+       //moves the h3 tag before the button
        ButtonName.parentNode.insertBefore(h3,ButtonName);
 
-    }
+       // Changes the text content of the firstParagraph, and makes it hidden
+       Paragraph1.textContent ="JavaScript is a prototype-based language, and every object in  JavaScript has a hidden internal property called Prototype that can be used to extend object properties and methods"
+       Paragraph1.style.visibility = "hidden";
+       //to check if paragraph1 exists and its contents
+       console.log(Paragraph1);
+
+  
+            
+      //Button click event listener with inline function
+      ButtonName.addEventListener("click",function()
+      {
+        if (ButtonName.textContent="Learn More")
+        {
+            ButtonName.textContent="Hide Details";
+        }  else 
+        {
+            ButtonName.textContent="Learn More";
+        }
+        
+      });
+  
+            
+      }
+       
+
+    
 
 
 
