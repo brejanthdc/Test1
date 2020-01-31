52e51b603f539b1e013f0bb9cfcc4c26f1371746
Brejanth Rajendran <Brejanth.rajendran@dcmail.ca>
2020-01-30 19:55:00 -0500

    Commit 3, Part 1 done

--- Scripts/app.js
+++ Scripts/app.js
@@ -25,7 +25,7 @@ let app;
     let h3 = document.createElement("h3");
     let contentArea = document.getElementById("contentArea");
 
-    let ButtonName = contentArea.getElementsByTagName("button");
+    let ButtonName = document.getElementById("largeButton");
     let Paragraph1 = document.getElementById("firstParagraph");
     function Start()
     {
@@ -41,16 +41,17 @@ let app;
     function Main()
     {
         console.log("%cDeclaring Variables", "color: red;");
-       // let ButtonName = contentArea.document
+       ButtonName.textContent ="Learn More";
       
        h3.setAttribute("class", "display-6");
        h3.textContent =" [Brejanth Rajendran] [100557484].";
        contentArea.appendChild(h3);
+
        firstParagraph.textContent ="JavaScript is a prototype-based language, and every object in  JavaScript has a hidden internal property called Prototype that can be used to extend object properties and methods"
-        firstParagraph.style.visibility = "hidden";
+       firstParagraph.style.visibility = "hidden";
        
        console.log(firstParagraph);
-      // contentArea.parentNode.insertBefore(h3,ButtonName);
+       ButtonName.parentNode.insertBefore(h3,ButtonName);
 
     }
 
